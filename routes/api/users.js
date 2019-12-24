b46cb78e7756cae460009e7b9fa8f8b94064b015
giorgi17 <giorgi199817@gmail.com>
2019-12-24 20:44:11 +0400

    eh

--- routes/api/users.js
+++ routes/api/users.js
@@ -17,21 +17,21 @@ const User = require("../../models/User");
 router.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'https://stock-twits-app.herokuapp.com');
   // res.setHeader('Access-Control-Allow-Origin', '*');
-  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  // next();
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // // Request methods you wish to allow
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // // Request headers you wish to allow
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    // // Set to true if you need the website to include cookies in the requests sent
+    // // to the API (e.g. in case you use sessions)
+    // res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
+    // // Pass to next layer of middleware
+    // next();
 });
 
 // @route POST api/users/stocktwits-login
