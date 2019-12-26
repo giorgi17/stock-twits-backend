bf80a88cdb50158c7bc54a6c65db13550d3baa90
giorgi17 <giorgi199817@gmail.com>
2019-12-26 22:23:35 +0400

    stocktwits oauth

--- routes/api/stocktwits-oauth.js
+++ routes/api/stocktwits-oauth.js
@@ -44,6 +44,7 @@ router.use(function (req, res, next) {
 router.get('/stocktwits-login', function(req, res) {
   // res.setHeader('Access-Control-Allow-Origin', 'https://stock-twits-app.herokuapp.com');
   req.headers['origin'] = 'https://stock-twits-app.herokuapp.com';
+  console.log(req);
   res.redirect('https://api.stocktwits.com/api/2/oauth/authorize?' +
     querystring.stringify({
       response_type: 'code',
